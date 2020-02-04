4ad30e65cb78d0b4cea11132c1e0c3e16390ab58
Andrew MacCann <andrew.maccann@gmail.com>
2020-02-04 11:40:51 +0000

    GH-32 Tweak demo-scene settings

--- packages/phaser3-drag-select/src/demo/scene/demo-scene-2.js
+++ packages/phaser3-drag-select/src/demo/scene/demo-scene-2.js
@@ -4,7 +4,7 @@ export default class DemoScene2 extends Phaser.Scene {
   }
 
   create() {
-    this.fpsText = this.add.text(500, 500, 'This is a scene with no drag-selection');
+    this.fpsText = this.add.text(500, 500, 'This is a scene with no drag-selection; press [R] to swap scenes');
     this.fpsText.setScrollFactor(0);
 
     this.input.keyboard.on('keydown-R', () => {
