0204b02c0e2e25dcde4a1762c470f52ca2007d4f
Andrew MacCann <andrew.maccann@gmail.com>
2020-02-04 16:54:54 +0000

    Fix exports

--- packages/phaser3-drag-select/src/index.exports.js
+++ packages/phaser3-drag-select/src/index.exports.js
@@ -1,7 +1,2 @@
-import DragSelectPlugin from './js/drag-select-plugin';
-import { PLUGIN_DEFAULT_CONFIG } from './js/lib/plugin-config';
-
-export default {
-  DragSelectPlugin,
-  PLUGIN_DEFAULT_CONFIG,
-};
+export { default as DragSelectPlugin } from './js/drag-select-plugin';
+export { PLUGIN_DEFAULT_CONFIG } from './js/lib/plugin-config';
