88b3855ec953715b21dc226b8c68f9ca65bfed14
Andrew MacCann <andrew.maccann@gmail.com>
2020-02-11 12:54:56 +0000

    GH-20 Global utils toolset
    - Basic project to store useful functions

---
+++ packages/phaser3-utils/webpack/base.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = require('./config');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: {
+    entry: `${config.DEMO}/index.js`,
+  },
+  resolve: {
+    alias: {
+      src: config.SRC,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader',
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        include: [config.ASSETS],
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+  ],
+};
